1943524a38b9cacf401c7996a64941c53cfa1722
Alexander Oronsaye <osahenrumeh13@gmail.com>
2025-02-09 14:09:53 +0100

    added video player for trending section using expo-video

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,50 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { useEvent } from "expo";
+
+const videoSource =
+  "https://cloud.appwrite.io/v1/storage/buckets/679b4fd200145714aa1a/files/67a89bd200177c40b032/view?project=679b4a2f000800e8c62f&mode=admin";
 
 const Bookmark = () => {
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.pause();
+  });
+
+  const { isPlaying } = useEvent(player, "playingChange", {
+    isPlaying: player.playing,
+  });
+
   return (
-    <View>
-      <Text>Bookmark</Text>
+    <View className="justify-center items-center flex-1  rounded-[35px] ">
+      <VideoView
+        style={{ width: 200, height: 250, borderRadius: 35 }}
+        player={player}
+        contentFit="cover"
+        allowsFullscreen
+        allowsPictureInPicture
+      />
+      <View style={styles.controlsContainer}>
+        <Button
+          title={isPlaying ? "Pause" : "Play"}
+          onPress={() => {
+            if (isPlaying) {
+              player.pause();
+            } else {
+              player.play();
+            }
+          }}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  video: {},
+  controlsContainer: {
+    padding: 10,
+  },
+});
 
 export default Bookmark;
--- components/Trending.jsx
+++ components/Trending.jsx
@@ -6,68 +6,115 @@ import {
   ImageBackground,
   TouchableOpacity,
   Image,
+  Button,
 } from "react-native";
 import React, { useState } from "react";
 import * as Animatable from "react-native-animatable";
 import { icons } from "../constants";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { useEvent } from "expo";
 
 const zoomIn = {
   0: {
-    opacity: 0,
-    scale: 0,
+    scale: 0.9,
   },
   1: {
-    opacity: 1,
-    scale: 1,
+    scale: 1.0,
   },
 };
+
 const zoomOut = {
   0: {
-    opacity: 1,
     scale: 1,
   },
   1: {
-    opacity: 0,
-    scale: 0,
+    scale: 0.9,
   },
 };
 
 const TrendingItem = ({ activeItem, item }) => {
   const [play, setPlay] = useState(false);
 
+  const videoSource =
+    "https://cloud.appwrite.io/v1/storage/buckets/679b4fd200145714aa1a/files/67a89bd200177c40b032/view?project=679b4a2f000800e8c62f&mode=admin";
+
+  const player = useVideoPlayer(videoSource, (player) => {
+    // player.loop = true;
+    player.pause();
+  });
+
+  const { isPlaying } = useEvent(player, "playingChange", {
+    isPlaying: player.playing,
+  });
+
   return (
     <Animatable.View
-      animation={activeItem === item.id ? zoomIn : zoomOut}
-      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      className="mr-5  "
       duration={500}
     >
-      {play ? (
-        <Text className="text-white">Playing</Text>
-      ) : (
-        <TouchableOpacity
-          onPress={() => setPlay(true)}
-          className=" justify-center items-center relative"
-        >
-          <ImageBackground
-            source={{ uri: item.thumbnail }}
-            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-black/40 shadow-lg "
+      <View className="w-52 h-72 justify-center items-center flex-1 rounded-[35px] ">
+        {!isPlaying ? (
+          <TouchableOpacity
+            className="relative flex justify-center items-center"
+            activeOpacity={0.7}
+            onPress={() => {
+              if (isPlaying) {
+                player.pause();
+              } else {
+                player.play();
+              }
+            }}
+          >
+            <ImageBackground
+              source={{
+                uri: item.thumbnail,
+              }}
+              className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+              resizeMode="cover"
+            />
+
+            <Image
+              source={icons.play}
+              className="w-12 h-12 absolute"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        ) : (
+          <VideoView
+            player={player}
+            className=" justify-center items-center   "
+            style={{ width: 200, height: 250, borderRadius: 35 }}
+            contentFit="contain"
+            allowsFullscreen
+            allowsPictureInPicture
           />
-          <Image className="w-12 h-12 absolute" source={icons.play} />
-        </TouchableOpacity>
-      )}
+        )}
+      </View>
     </Animatable.View>
   );
 };
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
   return (
     <FlatList
       data={posts}
+      horizontal
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <TrendingItem activeItem={item.id} item={item} />
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
-      horizontal
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{ x: 170 }}
     />
   );
 };
